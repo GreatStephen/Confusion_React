e14f8a3c06c581359f0ba5780a2bd65ef2b0983e
TianyouLi <litianyou97@gmail.com>
2020-10-17 00:35:20 -0400

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -20,12 +20,8 @@ class Main extends Component {
             comments: COMMENTS,
             promotions: PROMOTIONS,
             leaders: LEADERS
-            // selectedDish: null
         };
     }
-    // onDishSelect(dishId) {
-    //     this.setState({selectedDish: dishId});
-    // }
 
     render() {
         const HomePage = () => {
@@ -35,14 +31,21 @@ class Main extends Component {
                     leader={this.state.leaders.filter((l) => l.featured)[0]} />
             );
         };
+
+        const DishWithId = ({match}) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id===parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter(comment => comment.dishId===parseInt(match.params.dishId, 10))} />
+            );
+        }
+
         return (
             <div>
                 <Header />
-                {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/> */}
-                {/* <DishDetail dish={this.state.dishes.filter((dish) => dish.id===this.state.selectedDish)[0]} /> */}
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} /> } />
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
